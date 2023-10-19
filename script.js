d2fc6939638b842874703b47223906b2de548957
Amaral <105183282+Bsamarall@users.noreply.github.com>
2023-10-19 00:10:31 -0300

    Function e Alert
    Desafio do Curso em vídeo para treinar a utilização de function e alert.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Script com funções para cada botão.
+
+window.alert("Escolha um Botão")
+
+function funcaoB1() {
+    alert("Você clicou no Botão 1!")
+}
+
+function funcaoB2() {
+    alert("Você clicou no Botão 2!")
+}
+
+function funcaoB3() {
+    alert("Você clicou no Botão 3!")
+}
